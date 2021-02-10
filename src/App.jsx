d6fddec5a9f305b3390084ce5b87c075747002e5
Martijn van Rooijen <534599@student.glu.nl>
2021-02-10 13:30:57 +0100

    hi

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import mapStyles from '../mapStyles';
+import './App.css';
+import * as data from './data/markers.json';
+
+function Map(){
+    const [selectedMarker, setSelectedMarker] = useState(null);
+    const getFilename = (id) => data.actions[id].filename;
+    const getAction   = (id) => data.actions[id].action;
+
+    return <GoogleMap 
+    deaultZoom={12} 
+    defaultCenter={{lat:52.297901, lng:5.236660}}
+    defaultOptions={{styles: mapStyles}}>
+
+    {data.markers.map(marker =>
+        <Marker
+            key={marker.id}
+            position={{lat:marker.lat, lng:marker.lng}}
+            onClick={() => {
+                setSelectedMarker(marker);
+            }}
+            icon={{
+                url: `/dist/svg/${getFilename(marker.action)}`,
+                scaledSize: new window.google.maps.Size(30, 30)
+            }}
+        />)
+    }
+    {
+        selectedMarker && (
+           <InfoWindow
+               position={{
+                   lat: selectedMarker.lat,
+                   lng: selectedMarker.lng
+               }}
+               
+               onCloseClick={() =>{
+                selectedMarker(null);
+               }}
+               >
+               <div style={{width:"180px, overflow"}}>
+                   <h2>{getAction(selectedMarker.action)}</h2>
+                   <p>{selectedMarker.sender}</p>
+                   <p>{selectedMarker.desc}</p>
+               </div>
+           </InfoWindow> 
+        )
+    }
+    </GoogleMap>
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+    return (
+        <div style={{width:"100vw", height:"100vw"}}>
+            <WrappedMap 
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`} 
+            loadingElement={<div style={{height:"100%"}}/>}
+            containerElement={<div style={{height:"100%"}}/>}
+            mapElement={<div style={{height:"100%"}}/>}
+            />
+        </div>
+
+    )
+}
